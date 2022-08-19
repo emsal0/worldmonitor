a96e2f4594412fefb52447523104c5c79087f9e5
Emmanuel Sales <em@emsal.me>
2022-08-18 20:05:19 -0400

    More control of the world map.
    Commented out code gives more control over svg components with hast
    transforms.

--- src/app/country/country.component.ts
+++ src/app/country/country.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
+import { SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-country',
@@ -7,7 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CountryComponent implements OnInit {
 
+  countryId: string = '';
+  @Input() innerSvg: SafeHtml = '';
+  @Output() broadcastId = new EventEmitter<string>;
+
   constructor() { }
+
+  clickCountry() {
+    this.broadcastId.emit(this.countryId);
+  }
 
   ngOnInit(): void {
   }
--- src/app/worldmap/worldmap.component.ts
+++ src/app/worldmap/worldmap.component.ts
@@ -1,25 +1,31 @@
-import { Component, SimpleChanges, OnInit, OnChanges, Input } from '@angular/core';
+import { Component, SimpleChanges, ViewEncapsulation,
+  OnInit, OnChanges, Input } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { CountryComponent } from '../country/country.component';
 import { HttpClient } from '@angular/common/http';
 
-import { parse as svgParse } from 'svg-parser';
+import { matches as hMatches, select as hSelect,
+  selectAll as hSelectAll } from 'hast-util-select';
+import { HtmlParser } from '@starptech/webparser';
+import { toHtml } from 'hast-util-to-html'
+
+const fromWebparser = require('@starptech/hast-util-from-webparser');
 
 @Component({
   selector: 'app-worldmap',
   templateUrl: './worldmap.component.html',
-  styleUrls: ['./worldmap.component.less']
+  styleUrls: ['./worldmap.component.less'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class WorldmapComponent implements OnInit {
 
-  @Input() svg:SafeHtml = '';
-  country_cursor:string = '';
-  countries:Array<CountryComponent> = [];
-  parser:any;
+  @Input() svg: SafeHtml = '';
+  country_cursor: string = '';
+  countries: Array<CountryComponent> = [];
+  parser: HtmlParser = new HtmlParser({});
 
   constructor(
     private sanitizer: DomSanitizer,
-    private httpClient: HttpClient,
   ) { 
   }
 
@@ -27,7 +33,29 @@ export class WorldmapComponent implements OnInit {
   }
 
   ngOnChanges(changes: SimpleChanges): void {
-    const svgString = changes['svg'].currentValue['changingThisBreaksApplicationSecurity'];
-    console.log(svgParse(svgString).children[0]);
+    const svgString = changes['svg']
+      .currentValue['changingThisBreaksApplicationSecurity'];
+    let svgWebParse = this.parser.parse(svgString, '');
+    let svgHast = fromWebparser(svgWebParse.rootNodes).children[1];
+    
+    let oceanData = hSelect('.oceanxx', svgHast);
+    console.log(oceanData);
+    let countryData = hSelectAll(
+      'svg > path:has(title):not(.oceanxx),' +
+        'svg > g:has(title):not(.oceanxx)',
+      svgHast
+    );
+
+    //console.log(countryData);
+    //for (let countryElt of countryData) {
+      //console.log(toHtml(countryElt));
+    //}
+    //svgHast.children = [oceanData];
+    //console.log(toHtml(svgHast));
+    //this.svg = this.sanitizer.bypassSecurityTrustHtml(toHtml(svgHast));
+  }
+
+  clickCountry() {
+    console.log('clicked!');
   }
 }
