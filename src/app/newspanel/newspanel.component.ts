01cb03d1458f77057481aaedff8adf9a1b7f4db0
Emmanuel Sales <em@emsal.me>
2022-09-11 20:04:22 -0700

    Support multiple feeds

--- src/app/newspanel/newspanel.component.ts
+++ src/app/newspanel/newspanel.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { NewsService } from '../news.service';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-newspanel',
@@ -11,6 +12,8 @@ export class NewspanelComponent implements OnInit, OnChanges {
   @Input() country_data: any = {'title': 'unselected', 'id': 'xx'};
   @Input() rss_list: { [id: string]: string } = {};
   articles: Array<{title: string, link: string, content: string}> = [];
+  feedCounter: number = 0;
+  rssRequestSubscriptions: Array<Subscription> = [];
 
   constructor(private newsService: NewsService) { }
 
@@ -22,15 +25,38 @@ export class NewspanelComponent implements OnInit, OnChanges {
       let country_change = changes['country_data'].currentValue as
         {id: string, title: string};
       if(changes['country_data'].currentValue.id != 'xx') {
+        for (let sub of this.rssRequestSubscriptions) {
+            sub.unsubscribe();
+        }
+        this.feedCounter = 0;
         this.articles = [];
         let newId = country_change.id;
         console.log("newId: "+ newId);
-        let news_observable = this.newsService.
-            getArticles(this.rss_list[newId][0]);
-        news_observable.subscribe(arts => {
-            console.log(arts);
-            this.articles = arts;
-        });
+        for (let feedUrl of this.rss_list[newId]) {
+            let news_observable = this.newsService.
+                getArticles(feedUrl);
+            this.rssRequestSubscriptions.push(news_observable.subscribe(
+                arts => {
+                    console.log(arts);
+                    //this.articles = arts;
+                    let idx = 0;
+                    while (idx < this.articles.length) {
+                        idx += this.feedCounter;
+                        let nextArticle = arts.shift();
+                        if (nextArticle !== undefined) { 
+                            this.articles.splice(idx, 0, nextArticle);
+                        }
+                        idx += 1;
+                    }
+                    while (arts.length > 0) {
+                        let nextArticle = arts.shift();
+                        if (nextArticle !== undefined) { 
+                            this.articles.push(nextArticle);
+                        }
+                    }
+                    this.feedCounter += 1;
+                }));
+        }
       }
   }
 }
