f17c11a4d836e9d7a4cfa447585095968eb8f9b0
Emmanuel Sales <em@emsal.me>
2022-12-30 18:22:44 -0800

    (scuffed) countries now green if they have at least 1 source

--- js/src/app/country/country.component.ts
+++ js/src/app/country/country.component.ts
@@ -1,12 +1,15 @@
 import { Component, Input, OnInit, 
-  OnChanges, SimpleChanges } from '@angular/core';
+  OnChanges, SimpleChanges, OnDestroy } from '@angular/core';
 import { Output, EventEmitter } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 import { CountryData } from '../country-data';
+import { FeedListService} from '../feed-list.service';
 
 import { matches as hMatches, select as hSelect,
   selectAll as hSelectAll } from 'hast-util-select';
 import { toHtml } from 'hast-util-to-html'
+
+import { ReplaySubject, mergeMap, takeUntil } from 'rxjs';
 
 @Component({
   selector: '[svgCountry]',
@@ -17,13 +20,18 @@ export class CountryComponent implements OnInit {
 
   countryId: string = '';
   countryTitle: string = '';
+  hasSources: boolean = false;
   @Input() countryEltJson: any;
 
   innerSvg: SafeHtml = '';
   @Output() broadcastId = new EventEmitter<CountryData>;
 
+  countryIdEvent$ = new ReplaySubject<void>(1);
+  destroyed$ = new ReplaySubject<void>(1);
+
   constructor(
     private sanitizer: DomSanitizer,
+    private feedService: FeedListService, 
   ) { }
 
   clickCountry() {
@@ -31,16 +39,30 @@ export class CountryComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.countryIdEvent$.pipe(
+      takeUntil(this.destroyed$),
+      mergeMap( () => {
+        return this.feedService.getFeeds(this.countryId);
+      })
+    ).subscribe( (feeds: Array<string>) => {
+      if (feeds.length > 0) {
+        this.hasSources = true;
+      }
+    });
   }
 
   ngOnChanges(changes: SimpleChanges): void {
     this.innerSvg = this.sanitizer.bypassSecurityTrustHtml(
       toHtml(changes['countryEltJson'].currentValue));
-    this.countryId = this.countryEltJson.properties.id
+    this.countryId = this.countryEltJson.properties.id;
+    this.countryIdEvent$.next();
     let titleTag = hSelect('title', this.countryEltJson);
     if (titleTag != null) {
       this.countryTitle = (titleTag.children[0] as HTMLInputElement).value;
     }
   }
 
+  ngOnDestroy(): void {
+    this.destroyed$.next();
+  }
 }
